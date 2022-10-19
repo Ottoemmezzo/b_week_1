b83f30cf69bfecd661224e7d5cf3791e8c748871
SerenaMK <serenemudekoko@gmail.com>
2022-10-19 15:25:38 +0200

    aggiunto delle animazioni

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,30 @@
+// CAMBIO MENU SCROLL
+
+let section = document.querySelectorAll("section");
+let menu = document.querySelectorAll("nav a");
+
+window.onscroll = () => {
+  section.forEach((i) => {
+    let top = window.scrollY;
+    let offset = i.offsetTop - 250;
+    let height = i.offsetHeight;
+    let id = i.getAttribute("id");
+
+    if (top >= offset && top < offset + height) {
+      menu.forEach((link) => {
+        link.classList.remove("activeScroll");
+        document
+          .querySelector("nav a[href*=" + id + "]")
+          .classList.add("activeScroll");
+      });
+    }
+  });
+};
+
+
+
+
+
 // REVEAL
 
 function reveal() {
